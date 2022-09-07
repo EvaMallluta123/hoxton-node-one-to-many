01ee23fe74dcfea239ae7c92d643c30f856f2260
Eva Malluta <eva.malluta14@gmail.com>
2022-09-07 18:19:15 +0200

    change

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,13 @@ const getWorksforMuseum=db.prepare(`
 SELECT * FROM works WHERE museumId=@museumId
 `)
 
+const getWorks=db.prepare(`
+SELECT * FROM works
+`)
+
+const getMuseumsById=db.prepare(`
+SELECT * FROM museums WHERE id=@id;
+`)
 app.get(`/museums`, (req,res)=>{
 const museums=getMuseums.all()
 
@@ -31,6 +38,13 @@ app.get(`/museums/:id`, (req,res)=>{
 
 })
 app.get(`/works`, (req,res)=>{
+    const works=getWorks.all()
+
+    for(let work of works){
+        const museums=getMuseumsById.get({id: work.museumId})
+        work.museums=museums
+    }
+    res.send(works)
 
 })
 app.get(`/works/:id`, (req,res)=>{
