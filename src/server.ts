536487c2d22ecc44dc6c2ecda8f2d49b1b4665bd
Eva Malluta <eva.malluta14@gmail.com>
2022-09-07 20:10:46 +0200

    change

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,22 @@ SELECT * FROM works WHERE id=@id;
 const getMuseumsById = db.prepare(`
 SELECT * FROM museums WHERE id=@id;
 `);
+const createMuseum=db.prepare(`
+INSERT INTO museums
+(name, location, image)
+VALUES
+(@name,@location,@image)
+
+`)
+
+const createWork=db.prepare(`
+INSERT INTO work
+(name, picture, museumId)
+VALUES
+(@name, @picture, @museumId)
+
+
+`)
 app.get(`/museums`, (req, res) => {
   const museums = getMuseums.all();
 
@@ -37,6 +53,31 @@ app.get(`/museums`, (req, res) => {
   res.send(museums);
 });
 
+app.post(`/museums`, (req, res) => {
+
+    let errors: string[]=[]
+if (typeof req.body.name!==`string`){
+    errors.push(`Name is missing or not a string`)
+}
+if (typeof req.body.location!==`string`){
+    errors.push(`Location is missing or not a string`)
+}
+if (typeof req.body.image!==`string`){
+    errors.push(`Image is missing or not a string`)
+}
+
+if(errors.length===0){
+    const info=createMuseum.run(req.body)
+    const museum=getMuseumsById.get({id: info.lastInsertRowid})
+    const works = getWorksforMuseum.all({museumId: museum.id})
+    museum.works= works
+    res.send(museum)
+}
+else{
+    res.status(400).send({errors})
+}
+})
+
 app.get(`/museums/:id`, (req, res) => {
   const museum = getMuseumsById.get(req.params);
   if (museum) {
@@ -46,7 +87,7 @@ app.get(`/museums/:id`, (req, res) => {
   } else {
     res.status(400).send({ error: `Museum not found` });
   }
-});
+}),
 app.get(`/works`, (req, res) => {
   const works = getWorks.all();
 
@@ -66,6 +107,39 @@ app.get(`/works/:id`, (req, res) => {
       res.status(400).send({ error: `Works not found` });
     }
 });
+
+app.post(`/works`, (req, res) => {
+
+  let errors: string[]=[]
+if (typeof req.body.name!==`string`){
+  errors.push(`Name is missing or not a string`)
+}
+if (typeof req.body.picture!==`string`){
+  errors.push(`Picture is missing or not a string`)
+}
+if (typeof req.body.museumId!==`number`){
+  errors.push(`MuseumId is missing or not a number`)
+}
+
+if(errors.length===0){
+
+  const museum=getMuseumsById.get({id: req.body.museumId})
+  if(museum){
+    const info= createWork.run(req.body)
+    const work=getWorksById.get({id:info.lastInsertRowid})
+    work.museum=museum
+    res.send(work)
+  }
+  else{
+    res.status(400).send({
+      error:`You are creating an work for an museum that does not exist.`
+    })
+  }
+}
+else{
+  res.status(400).send({errors})
+}
+})
 
 app.listen(port, () => {
   console.log(`App running:http://localhost:${port}`);
